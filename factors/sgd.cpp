4d0d0eb152ba749b8e202f1d8e0658eaf33ad1e8
obinnae <binnixe@gmail.com>
2015-04-27 17:56:15 -0700

    simplified the gradient descent code. also added guards for u and v checks

--- factors/sgd.cpp
+++ factors/sgd.cpp
@@ -19,10 +19,14 @@ float * gradient(const float ** u,
      */
 
     Baseline b = new Baseline(d);
-    int u_index = b.user_id_from_entry_index(index);
-    int v_index = b.movie_id_from_entry_val(index);
 
-    float baseline_rating = b.get_baseline()
+    int u_index = d.user_id_from_entry_index(index);
+    int v_index = d.extract_movie_id(get_entry(index));
+    float rating = (float) d.extract_rating(get_entry(index));
+    //Would rather use a single entry_t object above and discard after this step
+
+    float baseline_rating = (float) b.get_baseline(u_index, v_index);
+
     float ** factor;
     float ** non_factor;
     float dot_prods[factor_length];
@@ -37,26 +41,36 @@ float * gradient(const float ** u,
         factor = v;
         non_factor = u;
     }
-
-    for (int j = 0, j < non_factor_width, j++){ //Need to find a way to only iterate through available vals
-
         /*
          * Loop that follows calculates the error arrising
          * from aproximating the rating using the factors
          */
 
-        float error = d.get_entry(index, j) - b.get_baseline(u_index, v_index); // Subtracting baseline from y here
-        for (int i = 0; i < factor_length; i++){
-            error -= u[index][i] + v[i][j];
-        }
-        for (int i = 0; i < factor_length; i++){
-            dot_prods[i] += (non_factor[i][j] * error);
-        }
-    }
+    float error = rating - b.baseline; // Subtracting baseline from y here
     for (int i = 0; i < factor_length; i++){
-        factor_gradient[i] = (lambda * factor[index][i]) - dot_prods[i];
+        error -= u[u_index][i] + v[i][v_index];
     }
 
+    if isU {
+    	for (int i = 0; i < factor_length; i++){
+    		dot_prods[i] += (non_factor[i][v_index] * error);
+    	}
+    }
+    else {
+    	for (int i = 0; i < factor_length; i++){
+    		dot_prods[i] += (non_factor[u_index][i] * error);
+    	}
+    }
+    if isU {
+    	for (int i = 0; i < factor_length; i++){
+    		factor_gradient[i] = (lambda * factor[u_index][i]) - dot_prods[i];
+    	}
+    }
+    else {
+    	for (int i = 0; i < factor_length; i++){
+    		factor_gradient[i] = (lambda * factor[i][v_index]) - dot_prods[i];
+    	}
+    }
     return factor_gradient;
 }
 
