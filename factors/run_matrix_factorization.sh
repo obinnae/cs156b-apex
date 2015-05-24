7592ae6c11b002ae1ae5c77225933a5b8d7d7b63
Obinna <binnixe@gmail.com>
2015-05-24 02:05:34 -0700

    commented out line that wasn't a bash directive

--- factors/run_matrix_factorization.sh
+++ factors/run_matrix_factorization.sh
@@ -2,7 +2,7 @@
 
 #./run_matrix_factorization ../data/train.cdta ../data/probe.cdta 10 1 0.01 0.008 ../data/qual_um.dta ../data/submission_10features_1epochs.dta
 ./run_matrix_factorization ../data/train.cdta ../data/probe.cdta 10 10 0.01 0.008 ../data/qual_um.dta ../data/submission_10features_10epochs.dta
-For quick submission test to see if modifications are any good on quiz
+#For quick submission test to see if modifications are any good on quiz
 
 ./run_matrix_factorization ../data/train.cdta ../data/probe.cdta 30 20 0.01 0.008 ../data/qual_um.dta ../data/submission_30features_20epochs.dta
 ./run_matrix_factorization ../data/train.cdta ../data/probe.cdta 50 20 0.01 0.008 ../data/qual_um.dta ../data/submission_50features_20epochs.dta
